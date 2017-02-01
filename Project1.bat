0fa2fea72bcdb9adfb2b6966e65b31591f42f978
svc-student <svc-student@student.directory.jccc>
2017-02-01 13:11:43 -0600

    File has been sent

---
+++ Project1.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\Unity\Editor\Data\Tools\UnityYAMLMerge.exe" merge -p "Blank.unity" "Blue.unity" "Red.unity" "End.unity"
+
+"C:\Program Files\Unity\Editor\Unity.exe" -quit -batchmode  -projectPath C:\Users\svc-student\Desktop\UnityExample -buildWindowsPlayer C:\Users\svc-student\Desktop\build.exe -quit
